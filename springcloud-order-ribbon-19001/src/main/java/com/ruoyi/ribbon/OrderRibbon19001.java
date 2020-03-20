b6475e94c71cf1216e4a0a54a4397606c8e249a5
heguangchuan <744645668@qq.com>
2020-03-20 15:33:58 +0800

    Ribbon负载均衡

--- springcloud-order-ribbon-19001/src/main/java/com/ruoyi/ribbon/OrderRibbon19001.java
+++ springcloud-order-ribbon-19001/src/main/java/com/ruoyi/ribbon/OrderRibbon19001.java
@@ -1,11 +1,12 @@
 package com.ruoyi.ribbon;
 
+import com.ruoyi.ribbon.conf.RibbonRuleConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 @EnableEurekaClient
-@RibbonClient(name = "payment-server")
+@RibbonClient(name = "payment-server", configuration = RibbonRuleConfig.class)
 @SpringBootApplication
 public class OrderRibbon19001 {
     public static void main(String[] args) {
