71377e3dd9c9d1d04ddbe9dd4e6e50b184258194
heguangchuan <744645668@qq.com>
2020-03-20 16:13:53 +0800

    springcloud-turbine

---
+++ springcloud-turbine-19006/src/main/java/com/ruoyi/turbine/Turbine19006.java
@@ -0,0 +1,17 @@
+package com.ruoyi.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableTurbine
+@EnableEurekaClient
+@EnableHystrixDashboard
+public class Turbine19006 {
+    public static void main(String[] args) {
+        SpringApplication.run(Turbine19006.class, args);
+    }
+}
