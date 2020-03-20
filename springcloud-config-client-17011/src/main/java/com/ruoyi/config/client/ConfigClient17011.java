e6911e8c10c61c2f90c0669f6caa4ff4a2cbf490
heguangchuan <596767880@qq.com>
2020-03-20 11:33:11 +0800

    配置中心客户端

---
+++ springcloud-config-client-17011/src/main/java/com/ruoyi/config/client/ConfigClient17011.java
@@ -0,0 +1,25 @@
+package com.ruoyi.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClient17011 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient17011.class, args);
+    }
+
+    @Value("${env}")
+    private String env;
+
+    @GetMapping
+    public String index() {
+        return env;
+    }
+}
