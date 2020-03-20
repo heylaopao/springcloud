fc0ec3ae0d5de4e440663162e9e74f12552a78f2
heguangchuan <596767880@qq.com>
2020-03-20 12:56:00 +0800

    spring cloud config 添加 spring cloud bus

--- springcloud-config-client-17011/src/main/java/com/ruoyi/config/client/ConfigClient17011.java
+++ springcloud-config-client-17011/src/main/java/com/ruoyi/config/client/ConfigClient17011.java
@@ -3,6 +3,7 @@ package com.ruoyi.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 @EnableEurekaClient
+@RefreshScope
 public class ConfigClient17011 {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClient17011.class, args);
